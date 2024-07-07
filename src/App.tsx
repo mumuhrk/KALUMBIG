b726e17252731529993a12404489c0472d2accc9
Sujinda Jaipinta <mumuch4321@gmail.com>
2024-07-07 15:45:46 +0700

    MAIN

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NINJA from "./assets/1123.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1> 788 </h1>
+      <img src={NINJA} width={500} height={500}></img>
+    </div>
+  );
+}
+export default App;
